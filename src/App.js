8bae6f7ea5a96ad76dbdbd830ee6c8048ca37b54
Bhavya Verma <vbhavya269@gmail.com>
2022-06-20 00:36:31 +0530

    ✨ Deleting docs from Firestore DB

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { db } from './firebase-config';
-import { collection, getDocs, addDoc, updateDoc, doc } from 'firebase/firestore'
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore'
 
 function App() {
   const [newName, setNewName] = useState("");
@@ -18,6 +18,11 @@ function App() {
     const userDoc = doc(db, "users", id)
     const newFields = { age: age + 1 }
     await updateDoc(userDoc, newFields)
+  }
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id)
+    await deleteDoc(userDoc)
   }
 
   useEffect(() => {
@@ -53,7 +58,8 @@ function App() {
         return (<div key = {user.id}>
           <h1>Name: {user.name}</h1>
           <h1>Age: {user.age}</h1>
-          <button onClick={ () => updateUser(user.id, user.age)}>Increase Age</button>
+          <button onClick={() => updateUser(user.id, user.age)}>Increase Age</button>
+          <button onClick={() => deleteUser(user.id)}>Delete User</button>
         </div>)
       })}
     </div>
